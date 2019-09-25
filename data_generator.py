7d491831f09346878781b9018d595956ff661c2a
Aman Agarwal <amanag.11@gmail.com>
2019-09-25 11:04:49 +0530

    Update data_generator.py

--- data_generator.py
+++ data_generator.py
@@ -7,10 +7,10 @@ class Siamese_Loader:
     def __init__(self, path, data_subsets=["train", "val"]):
         
         # gets all file paths (separate for each person)
-        self.file_set = [file for file in (glob.glob(path + '/ecg-id-database-mod/Person_{:02d}/rec_*.png'.format(num)) for num in range(1, 91))]
+        self.file_set = [file for file in (glob.glob(path + '/ecg-id-database-filter/Person_{:02d}/rec_*.png'.format(num)) for num in range(1, 91))]
         
         # defining train and val set
-        self.val_set = [89, 74, 13, 1, 38, 52]
+        self.val_set = [1, 6, 13, 15, 21, 25, 29, 38, 41, 44, 52, 62, 68, 74, 80, 89]
         self.train_set = list(range(1, 91))
         for ele in sorted(self.val_set, reverse=True):  
             del self.train_set[ele-1]
@@ -26,7 +26,7 @@ class Siamese_Loader:
         w, h = 144, 224
 
         #initialize 2 empty arrays for the input image batch
-        pairs = [np.zeros((batch_size, w, h, 3)) for i in range(2)]
+        pairs = [np.zeros((batch_size, w, h, 1)) for i in range(2)]
         #initialize vector for the targets, and make one half of it '1's, so 2nd half of batch has same class
         targets = np.zeros((batch_size,))
         targets[batch_size//2:] = 1
@@ -40,7 +40,7 @@ class Siamese_Loader:
             # load and format the image
             img = Image.open(pair_x)
             img = img.resize((h, w))
-            pairs[0][i,:,:,:] = np.array(img)[:,:,:3] / 255
+            pairs[0][i,:,:,:] = np.array(img)[:,:,0:1] / 255
 
             #pick images of same class for 1st half, different for 2nd
             if i < batch_size // 2:
@@ -48,7 +48,7 @@ class Siamese_Loader:
 
             img = Image.open(pair_y)
             img = img.resize((h, w))
-            pairs[1][i,:,:,:] = np.array(img)[:,:,:3] / 255
+            pairs[1][i,:,:,:] = np.array(img)[:,:,0:1] / 255
 
         return pairs, targets
     
@@ -73,28 +73,28 @@ class Siamese_Loader:
         true_idx = idx[0]      # test person
         false_idx = idx[1:]    # random people
 
-        pairs = [np.zeros((N, w, h, 3)) for i in range(2)]
+        pairs = [np.zeros((N, w, h, 1)) for i in range(2)]
 
         # contains N-1 sample of random people, 1 sample of test person
-        support_set = np.zeros((N, w, h, 3))
+        support_set = np.zeros((N, w, h, 1))
         # contains N sample of test person (1 person)
-        test_image = np.zeros((N, w, h, 3))
+        test_image = np.zeros((N, w, h, 1))
 
         targets = np.zeros((N,))
         targets[0] = 1
 
         # gets file path of N+1 sample of test person
         pair_x = np.random.choice(self.file_set[true_idx-1], N+1, replace=False).tolist()
-        support_set[0] = np.array(Image.open(pair_x[0]).resize((h, w)))[:,:,:3] / 255
+        support_set[0] = np.array(Image.open(pair_x[0]).resize((h, w)))[:,:,0:1] / 255
 
         j = 0
         for i in pair_x[1:]:
-          test_image[j] = np.array(Image.open(i).resize((h, w)))[:,:,:3] / 255
+          test_image[j] = np.array(Image.open(i).resize((h, w)))[:,:,0:1] / 255
           j += 1
 
         for i in range(1, N):
           pair_y = np.random.choice(self.file_set[false_idx[i-1]], 1, replace=False)[0]
-          support_set[i] = np.array(Image.open(pair_y).resize((h, w)))[:,:,:3] / 255
+          support_set[i] = np.array(Image.open(pair_y).resize((h, w)))[:,:,0:1] / 255
             
         # shuffle data
         targets, test_image, support_set = shuffle(targets, test_image, support_set)
@@ -114,7 +114,7 @@ class Siamese_Loader:
             probs = model.predict(inputs).reshape((N,))
 
             # check accuracy
-            acc.append((((probs > 0.5)*1)==targets).mean())
+            acc.append(np.argmax(probs)==np.argmax(targets))
             
         percent_correct = 100*sum(acc)/len(acc)
 
