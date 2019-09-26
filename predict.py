ebba266d35fe2e3d4af5c5836c4d1f1a2a4e1959
Aman Agarwal <amanag.11@gmail.com>
2019-09-26 15:16:13 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -3,16 +3,19 @@ import numpy as np
 from PIL import Image
 from tensorflow.keras.models import load_model
 
-records = sorted(glob.glob('path-to-data/ecg-id-database-mod/Person_*/rec_*.png'))
+records = sorted(glob.glob('path-to-data/ecg-id-database-filter/Person_*/rec_*.png'))
 model = load_model('model_ecg.h5')
 w, h = 144, 224
 
-person_1 = np.array(Image.open(records[0]).resize((h, w)))[:,:,:3] / 255
-person_2 = np.array(Image.open(records[100]).resize((h, w)))[:,:,:3] / 255
-person_3 = np.array(Image.open(records[200]).resize((h, w)))[:,:,:3] / 255
-person_3_test = np.array(Image.open(records[201]).resize((h, w)))[:,:,:3] / 255
+def read_image(img):
+  retrun np.array(Image.open(img).resize((h, w)))[:,:,0:1] / 255
 
-prob = model.predict([person_1.reshape((1, w, h, 3)), person_3_test.reshape((1, w, h, 3))])
+person_1 = read_image(records[0])
+person_2 = read_image(records[100])
+person_3 = read_image(records[200])
+person_3_test = read_image(records[201])
+
+prob = model.predict([person_1.reshape((1, w, h, 1)), person_3_test.reshape((1, w, h, 1))])
 pred = (prob>0.5)[0][0]
 if pred:
   print("Person verified [confidence: {:.2f}%]".format(100*prob[0][0]))
